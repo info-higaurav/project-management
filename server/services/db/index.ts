a60c6d1099fc4be48aba2093c7aef3c863645d1c
info.higaurav <ai.higaurav@gmail.com>
2024-12-15 01:31:59 +0530

    Refactor database connection logic to support different environments
    - Updated the database connection function to use separate URIs for production and local environments based on NODE_ENV.
    - Ensured the connection is established with the appropriate database URI for improved deployment flexibility.

--- server/services/db/index.ts
+++ server/services/db/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
 export default async function connectDb (){
-   const res = await mongoose.connect(process.env.DB_URI as string, {dbName:"project-managment"})
+   const res = await mongoose.connect(process.env.NODE_ENV === "production" ? process.env.PRODUCTION_DB_URI as string : process.env.LOCAL_DB_URI as string , {dbName:"project-managment"})
    return res.connection.host;
 }
