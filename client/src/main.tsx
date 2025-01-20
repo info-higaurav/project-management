1e44808cf58e0663c5bd9cac0004819b93037208
Gaurav Kumar <ai.higaurav@gmail.com>
2025-01-21 01:49:56 +0530

    Update main.tsx
    export query Client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import {
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 // Create a client
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
